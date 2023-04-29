4c700ee6189942d2d6544b7a8846deea65fd0514
Amitendra Singh <63096147+amitendra9@users.noreply.github.com>
2023-04-29 16:42:53 +0000

    add more in intro

---
+++ Introduction/01_intro_part2.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# first command and print through echo command 
+echo "Introduction of Shell Scripts";
+
+# variable 
+
+# ${foo:0:3}	Substring (position, length)
+# ${foo:(-3):3}	Substring from the right
+
+name="Amitendra";
+echo "${name}"
+echo "${name/A/a}"    #=> "amitendra" (substitution)
+echo "${name:0:2}"    #=> "Am" (slicing)
+echo "${name::2}"     #=> "Am" (slicing)
+echo "${name::-1}"    #=> "Amitendr" (slicing)
+echo "${name:(-1)}"   #=> "a" (slicing from right)
+echo "${name:(-2):1}" #=> "r" (slicing from right)
+echo "${food:-Cake}"  #=> $food or "Cake"
+
+
+
+# Substitution
+
+str="/path/to/foo.cpp"
+echo "${str%.cpp}"    # /path/to/foo
+echo "${str%.cpp}.o"  # /path/to/foo.o
+echo "${str%/*}"      # /path/to
+
+echo "${str##*.}"     # cpp (extension)
+echo "${str##*/}"     # foo.cpp (basepath)
+
+echo "${str#*/}"      # path/to/foo.cpp
+echo "${str##*/}"     # foo.cpp
+
+echo "${str/foo/bar}" # /path/to/bar.cpp
+
+: '
+
+# ${foo%suffix}	    Remove suffix
+# ${foo#prefix}	    Remove prefix
+# ${foo%%suffix}	Remove long suffix
+# ${foo/%suffix}	Remove long suffix
+# ${foo##prefix}	Remove long prefix
+# ${foo/#prefix}	Remove long prefix
+# ${foo/from/to}	Replace first match
+# ${foo//from/to}	Replace all
+# ${foo/%from/to}	Replace suffix
+# ${foo/#from/to}	Replace prefix
+'
